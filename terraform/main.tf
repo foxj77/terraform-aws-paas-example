b0286a6b0aac650f8936a23d14f1dcd908c5a7be
John Fox <jfox@experfy.com>
2022-03-08 17:06:04 +0000

    adding network

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,18 @@ resource "azurerm_resource_group" "rg" {
     "productowner" = "JohnFox"
   } 
 }
+
+module "network" {
+  source                      = "./modules/network"
+  environment                 = var.prefix
+  customer                    = var.customer
+  project                     = var.project
+  resource_group_name         = azurerm_resource_group.main.name
+  subnet_cidr                 = var.subnet_cidr
+  address_space               = var.address_space
+  location                    = var.location
+  az                          = var.az
+  nat_gateway_id              = module.nat-gateway.nat_gateway_id
+  network_security_group_name = module.nsg.network_security_group_name
+  network_security_group_id   = module.nsg.network_security_group_id
+}

c8a24a63df99cd9364a19c3c7362213bfab56b0f
John Fox <jfox@experfy.com>
2022-03-08 15:16:26 +0000

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "cloudreach-johnfoxdemo"
+  location = "westus2"
+}

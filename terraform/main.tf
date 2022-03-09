623252422d19667e79e10006eb27b2606a5e0579
John Fox <jfox@experfy.com>
2022-03-09 11:10:29 +0000

    removing one sql db

--- terraform/main.tf
+++ terraform/main.tf
@@ -106,39 +106,3 @@ resource "azurerm_mysql_flexible_server" "example" {
 }
 
 
-resource "azurerm_mysql_server" "example" {
-  name                = "example-mysqlserver"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  administrator_login          = "mysqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
-
-  sku_name   = "GP_Standard_D2ds_v4"
-  storage_mb = 5120
-  version    = "5.7"
-
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
-}
-
-resource "azurerm_mysql_firewall_rule" "example" {
-  name                = "azureAccess"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_mysql_server.example.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
-
-resource "azurerm_mysql_firewall_rule" "middleware" {
-  name                = "middlewareAccess"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_mysql_server.example.name
-  start_ip_address    = "10.0.2.0"
-  end_ip_address      = "10.0.2.255"
-}

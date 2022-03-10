40bd7038f484fa0c24c45aa3fad80adb123981e9
John Fox <jfox@experfy.com>
2022-03-10 12:47:42 +0000

    change to vnet name

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,6 +1,6 @@
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "snapvideo" {
+  name                = "snapvideoNetwork"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]

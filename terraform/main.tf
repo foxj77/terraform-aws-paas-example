ee89130b706ae89ba34af74ac207b175abcb6c74
John Fox <jfox@experfy.com>
2022-03-10 12:03:33 +0000

    change to subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -118,7 +118,7 @@ resource "azurerm_subnet" "database" {
   name                 = "databaseSubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.4.0/24"]
+  address_prefixes     = ["10.0.3.0/24"]
   service_endpoints    = ["Microsoft.Storage"]
   delegation {
     name = "fs"

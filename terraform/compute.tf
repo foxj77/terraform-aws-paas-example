7291b228a4d7e6c57d9d70d12e1c317540e6ca44
John Fox <jfox@experfy.com>
2022-03-11 21:54:56 +0000

    Update compute.tf

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -6,10 +6,10 @@ resource "azurerm_virtual_machine" "snapvideoweb" {
   vm_size               = "Standard_B2ms"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+  delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"

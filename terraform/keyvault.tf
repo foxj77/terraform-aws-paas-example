d050b0d0c2c30862ce7edae5ed5344b031cc8d60
John Fox <jfox@experfy.com>
2022-03-11 22:27:49 +0000

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault"
+resource "azurerm_key_vault" "kv" {
+  name                        = "snapVideoKeyVault"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true

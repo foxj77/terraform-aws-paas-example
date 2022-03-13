4f9a206f783c40e2adab75bcf5fc794b9880b1f6
John Fox <jfox@experfy.com>
2022-03-13 22:28:11 +0000

    Update database.tf

--- terraform/database.tf
+++ terraform/database.tf
@@ -8,10 +8,12 @@ resource "azurerm_mysql_flexible_server" "snapvideo" {
   delegated_subnet_id    = azurerm_subnet.database.id
   private_dns_zone_id    = azurerm_private_dns_zone.snapvideo.id
   sku_name               = "GP_Standard_D2ds_v4"
+  zone                   = 1
 
   high_availability {
-    mode = "ZoneRedundant"
+    mode                      = "ZoneRedundant"
+    standby_availability_zone = 2
   }
 
-  depends_on = [azurerm_private_dns_zone_virtual_network_link.snapvideo, azurerm_subnet.database,azurerm_key_vault.kv]
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.snapvideo, azurerm_subnet.database, azurerm_key_vault.kv]
 }

cc74edb5f267944ae288f012d4fedee75b1e6f42
John Fox <jfox@experfy.com>
2022-03-14 12:32:55 +0000

    Update keyvault.tf

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
-  name                        = "kv-${var.customer}-${terraform.workspace}"
+  name                        = "kv-${var.customer}-${terraform.workspace}-weur"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
